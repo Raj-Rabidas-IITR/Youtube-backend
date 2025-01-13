8e7bd8d19b3412dfcc6f580bbfc6dc3240bfb741
Raj-Rabidas-IITR <rajrabidas001@gmail.com>
2025-01-13 12:23:43 +0530

    userrmodel-change

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
 
 
 const userSchema=new Schema({
@@ -57,6 +57,25 @@ const userSchema=new Schema({
 
 },{timeseries:true})
 
-userSchema.pre("save")
+
+
+// built in hooks 
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+    
+})
+
+
+// custom hooks 
+userSchema.methods.isPasswordCorrect=async function (password) {
+   return await bcrypt.compare(password,this.password)
+
+
+    
+}
+
 
 export const User= mongoose.model("User",userSchema);
